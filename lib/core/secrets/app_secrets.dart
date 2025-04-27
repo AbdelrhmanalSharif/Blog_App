deea588883aaea4e59016b5215722ea9414fff9c
Abd El Rhman <abedelrahmanalsharif31@gmail.com>
2025-04-27 22:49:33 +0300

    new supabase

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppScrets {
-  static const supabaseUrl = 'https://zxlgznjdglhcjcqwgggo.supabase.co';
+  static const supabaseUrl = 'https://lbrecgaljscoruebkmta.supabase.co';
   static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4bGd6bmpkZ2xoY2pjcXdnZ2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQzMzA5NzUsImV4cCI6MjAyOTkwNjk3NX0.goa11wVZdY0lMgeOFN8m73rxq8GxYTvgZyEKr21C59Y';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxicmVjZ2FsanNjb3J1ZWJrbXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3ODIxNzgsImV4cCI6MjA2MTM1ODE3OH0.0e-3itTAbi7aVG2smu7OqqfBMnzQ7JcIff-Mq38NPuA';
 }
